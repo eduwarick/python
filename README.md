a5b220b64e1090220c3c411546cf1c81ce05884d
Eduardo Warick <warick.eduardo@gmail.com>
2019-04-02 12:48:46 -0300

    Update to variable learning.

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This is a repo to store my lessons using python.
 
 List of things done:
 1. Hello World
-2. 
+2. Variable testing
+3. String Manipulation
+4. 
--- variables.py
+++ variables.py
@@ -8,3 +8,27 @@ print(x)
 
 y = ", World!"
 print(x + y)
+
+#Type test
+##Integer
+x = 5
+print (x)
+print(type(x))
+
+##Float
+x = 5.1
+print (x)
+print(type(x))
+
+x = 5e3
+print (x)
+print(type(x))
+
+##Complex
+x = 5+1j
+print (x)
+print(type(x))
+
+#Casting test
+x = float(1)
+print (x)
